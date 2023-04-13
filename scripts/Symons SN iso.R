981e2b6211742488cd58a609a27ea0001adef2a1
cbwall <cbw0047gmail.com>
2023-04-13 16:28:02 -0700

    updates with new zoops SIA data

--- scripts/Symons SN iso.R
+++ scripts/Symons SN iso.R
@@ -1,18 +1,29 @@
-iso<-read.csv("isotope test/d13C_isotopes.csv")
+##### Bulk stable isotope analyses (SIA)
+library("ggplot2")
+iso<-read.csv("data/Symons iso data/Symons_Yos_bulkSIA.csv")
 
 str(iso)
 
 # elevation
-plot<-ggplot(iso, aes(x=d13C, y=elevation.m, color=type)) +
+d13C.elevation<-ggplot(iso, aes(x=d13C, y=elevation.m, color=type)) +
+  scale_color_manual(values=c("orchid", "lightgoldenrod3", "mediumseagreen", "royalblue2", "coral")) +
+  xlab(expression(paste(delta^{13}, C, " (\u2030, V-PDB)")))+
+  ylab("Elevation (m)")+
+  labs(color = "")+
+  coord_cartesian(xlim=c(-45, -5), ylim=c(2400, 3500)) +
   geom_point() +
   stat_ellipse()+
-  theme_bw()
-ggsave("isotope test/13C.elevation.pdf", height=7, width=7)
+  theme_classic()
+ggsave("figures/Symons data plots/Bulk_13C.elevation.pdf", encod="MacRoman", height=6, width=7)
 
 #C vs N
 
-plot2<-ggplot(iso, aes(x=d13C, y=d15N, color=type)) +
+d13C.d15N<-ggplot(iso, aes(x=d13C, y=d15N, color=type)) +
+  scale_color_manual(values=c("orchid", "lightgoldenrod3", "mediumseagreen", "royalblue2", "coral")) +
+  xlab(expression(paste(delta^{13}, C, " (\u2030, V-PDB)")))+
+  ylab(expression(paste(delta^{15}, N, " (\u2030, Air)")))+
+  coord_cartesian(xlim=c(-45, -5), ylim=c(-6, 10)) +
   geom_point() +
   stat_ellipse()+
-  theme_bw()
-ggsave("isotope test/13C.15N.pdf", height=7, width=7)
+  theme_classic()
+ggsave("figures/Symons data plots/Bulk_13C.15N.pdf", encod="MacRoman", height=6, width=7)
